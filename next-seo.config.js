464f7195bfc91361919760efe96b0afe46605df6
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-07-31 16:15:11 +0530

    Initial commit

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+const seoConfig = {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    site_name: '',
+  },
+  twitter: {
+    handle: '@_Muthukumar_P',
+    site: '@_Muthukumar_P',
+    cardType: 'summary_large_image',
+  },
+  additionalMetaTags: [
+    {
+      name: '',
+      content: '',
+    },
+  ],
+  additionalLinkTags: [
+    {
+      rel: '',
+      href: '',
+    },
+  ],
+}
+
+export default seoConfig
