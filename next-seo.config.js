c6f7bc09ad23c7df5afce5941539833cc5687753
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-08-01 17:07:02 +0530

    feat: add seo stuff

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,11 +2,11 @@ const seoConfig = {
   openGraph: {
     type: 'website',
     locale: 'en_IE',
-    site_name: '',
+    site_name: 'Create translation strings',
   },
   twitter: {
-    handle: '@_Muthukumar_P',
-    site: '@_Muthukumar_P',
+    handle: '@am_muthukumar',
+    site: '@am_muthukumar',
     cardType: 'summary_large_image',
   },
   additionalMetaTags: [
@@ -17,8 +17,29 @@ const seoConfig = {
   ],
   additionalLinkTags: [
     {
-      rel: '',
-      href: '',
+      rel: 'apple-touch-icon',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon-32x32.png',
+      sizes: '32x32',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon-16x16.png',
+      sizes: '16x16',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: '#5bbad5',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
     },
   ],
 }
